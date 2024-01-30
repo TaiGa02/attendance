d70759ba120e5505f4368d4a8d9c92f9d95c3d40
TaiGa02 <d-taiga.4869@outlook.jp>
2024-01-30 21:24:04 +1000

    try

--- src/app/resultAdmin/page.tsx
+++ src/app/resultAdmin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Nav from "../../components/Nav";
 import useSWR from "swr";
@@ -71,6 +71,7 @@ export default function Admin() {
 
   return (
     <>
+     <Suspense fallback={<div>Loading...</div>}>
       <Nav onSubmit={handleSubmit} />
       <main>
         <div className="flex flex-col text-center">
@@ -94,6 +95,7 @@ export default function Admin() {
           </table>
         </div>
       </main>
+      </Suspense>
     </>
   );
 }
